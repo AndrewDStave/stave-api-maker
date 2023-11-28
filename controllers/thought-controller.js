f0d7cfe5561b4819e09aa48770346d5aec8a7fc3
Andrew Stave <andrewdstave@gmail.com>
2023-11-28 14:54:23 -0800

    finished controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,6 +12,48 @@ const thoughtController = {
     }
   },
 
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  getThoughtById({ params }, res) {
+    Thought.findById({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  updateThought({ params, body }, res) {
+    Thought.findByIdAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteThought({ params }, res) {
+    Thought.findByIdAndDelete({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
   createReaction({ params, body }, res) {
     Thought.findByIdAndUpdate(
       { _id: params.thoughtId },
